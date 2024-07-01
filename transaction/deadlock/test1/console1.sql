028df7221a0519b77ea63b0cd1e9ce598dda1b4a
hku2002 <hku2002@hanmail.net>
2024-07-01 22:24:39 +0900

    feat: 순번 추가

--- transaction/deadlock/test1/console1.sql
+++ transaction/deadlock/test1/console1.sql
@@ -1,10 +1,10 @@
--- 트랜잭션 시작
+-- 1. 트랜잭션 시작
 START TRANSACTION;
 
--- PRODUCT 테이블에 값 삽입
+-- 2. PRODUCT 테이블에 값 삽입
 UPDATE PRODUCT SET name = 'PRODUCT-01' WHERE id = 1;
 
--- ITEM 테이블에 값 삽입
+-- 5. ITEM 테이블에 값 삽입
 UPDATE ITEM SET name = 'ITEM-01' WHERE id = 1;
 
 -- 트랜잭션 커밋
--- transaction/deadlock/test1/console2.sql
+++ transaction/deadlock/test1/console2.sql
@@ -1,10 +1,10 @@
--- 트랜잭션 시작
+-- 3. 트랜잭션 시작
 START TRANSACTION;
 
--- ITEM 테이블에 값 삽입
+-- 4. ITEM 테이블에 값 삽입
 UPDATE ITEM SET price = 1001 WHERE id = 1;
 
--- PRODUCT 테이블에 값 삽입
+-- 6. PRODUCT 테이블에 값 삽입
 UPDATE PRODUCT SET name = 'trx test' WHERE id = 1;
 
 -- 트랜잭션 커밋
