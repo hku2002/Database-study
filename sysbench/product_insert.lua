ac753040f6217895923c0e42bac40a02808732e2
hku2002 <hku2002@hanmail.net>
2024-08-16 20:56:48 +0900

    refactor: 스크립트 수정

--- sysbench/product_insert.lua
+++ sysbench/product_insert.lua
@@ -1,16 +1,20 @@
--- 데이터베이스 접속 정보 설정
+-- product_insert.lua
+
+-- 데이터베이스 접속 초기화
 function thread_init(thread_id)
    db_connect()
 end
 
--- 각 스레드에서 실행할 작업 정의
+-- 각 스레드에서 실행할 이벤트
 function event()
-   -- INSERT 쿼리 예시
-   local table_name = "PRODUCT"
+   local id = sysbench.rand.default(1, 1000000)
    local name = string.format("Product_%d", id)
    local price = sysbench.rand.default(100, 10000)
-   local query = string.format("INSERT INTO PRODUCT (id, name, price) VALUES (%d, '%s', %d)",
-                                  id, name, price)
 
+   -- id, name, price 값을 삽입하는 쿼리
+   local query = string.format("INSERT INTO PRODUCT (name, price) VALUES ('%s', %d)",
+                               name, price)
+
+   -- 쿼리 실행
    db_query(query)
 end
