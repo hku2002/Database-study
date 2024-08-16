05792b6aaa59bf18e88004a4a519941ed068babb
hku2002 <hku2002@hanmail.net>
2024-08-16 20:56:34 +0900

    feat: 스크립트 시작 sh 추가

---
+++ sysbench/insert_start.sh
@@ -0,0 +1,11 @@
+
+sysbench ./product_insert.lua \
+  --db-driver=mysql \
+  --mysql-host=127.0.0.1 \
+  --mysql-port=3306 \
+  --mysql-user=root \
+  --mysql-password=1234 \
+  --mysql-db=study \
+  --threads=1 \
+  --events=100000 \
+  run
